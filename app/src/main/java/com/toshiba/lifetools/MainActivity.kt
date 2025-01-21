f9ed051a87ea3404b7f9af83458989fb091bc80f
Toshiba-arch <toshiba.sala@gmail.com>
2025-01-21 00:10:36 +0000

    configur inicial + layout euromilhoes

--- app/src/main/java/com/toshiba/lifetools/MainActivity.kt
+++ app/src/main/java/com/toshiba/lifetools/MainActivity.kt
@@ -1,35 +1,45 @@
 package com.toshiba.lifetools
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+// import androidx.activity.ComponentActivity
+// import androidx.activity.compose.setContent
+// import androidx.activity.enableEdgeToEdge
+// import androidx.compose.foundation.layout.fillMaxSize
+// import androidx.compose.foundation.layout.padding
+// import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.toshiba.lifetools.ui.theme.LifeToolsAppTheme
+import android.content.Intent
+// import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            LifeToolsAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        // Configurar o botão para a ferramenta "Euromillions"
+        val btnEuromillions = findViewById<Button>(R.id.btn_euromillions)
+        btnEuromillions.setOnClickListener {
+            // Navegar para EuromillionsActivity
+            val intent = Intent(this, EuromillionsActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Exemplo: Adicionar mais botões para outras ferramentas
+        val btnOtherTool = findViewById<Button>(R.id.btn_other_tool)
+        btnOtherTool.setOnClickListener {
+            // Navegar para OtherToolActivity
+            val intent = Intent(this, OtherToolActivity::class.java)
+            startActivity(intent)
         }
     }
 }
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
